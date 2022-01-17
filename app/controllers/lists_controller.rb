8717c44cc0c25c88ed8b11ce06a39e6e3850509f
Asaoka-kazuya <ookami19970418@gmail.com>
2022-01-17 11:46:29 +0000

    [finish]4-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,17 @@
 class ListsController < ApplicationController
+  
   def new
+    @list = List.new
+  end
+  
+  
+  def create
+    #1.&2.データ受け取り新規登録するためのインスタンス作成 
+    list = List.new(list_params)
+    
+    list.save
+    
+    redirect_to'/top'
   end
 
   def index
@@ -9,5 +21,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
